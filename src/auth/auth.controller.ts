2bc85df97db230cb365ab065d1c86727a66e7285
Kucenko Nikita <kucenko.nikita@gmail.com>
2023-01-05 19:25:38 +0700

    Add jwt service for authorization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Авторизация')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  login() {
-    return 'Login';
+  login(@Body() createUserDto: CreateUserDto) {
+    return this.authService.login(createUserDto);
   }
 
   @Post('/registration')
-  registration() {
-    return 'Registration';
+  registration(@Body() createUserDto: CreateUserDto) {
+    return this.authService.registration(createUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(createUserDto: CreateUserDto) {}
+
+  async registration(createUserDto: CreateUserDto) {
+    const candidate = await this.userService.getUserByEmail(
+      createUserDto.email,
+    );
+  }
+}
