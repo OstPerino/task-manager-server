241943259c36b0c77ebb7113245c339d2585c09d
Nikita <kucenko.nikita@gmail.com>
2023-02-27 02:22:57 +0700

    Add field of foreign key in dto of board

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -3,4 +3,7 @@ import {ApiProperty} from "@nestjs/swagger";
 export class CreateBoardDto {
     @ApiProperty({ example: 'Board Name', description: 'Название' })
     readonly name: string;
+
+    @ApiProperty({ example: 'ID of project', description: 'ID проекта' })
+    readonly projectId: number;
 }
